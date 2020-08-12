025399ada48908eff222f12d89651b183a6e14c4
Rahul Parkar <rahul.a.parkar@gmail.com>
2020-08-12 22:38:16 +0100

    Add new fields to generate token

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -3,7 +3,9 @@ import { sign } from 'jsonwebtoken'
 export default function generateToken(user) {
   const data =  {
     id: user.id,
-    username: user.name,
+    username: user.username,
+    email: user.email,
+    bybitRegistration: user.bybitRegistration
   };
   const signature = process.env.TOKEN_SIGNATURE;
   const expiration = process.env.TOKEN_LIFETIME;
