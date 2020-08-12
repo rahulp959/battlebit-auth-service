507c2c2cc45f8c6a47262fdeef63aa63b3142d8b
Rahul Parkar <rahul.a.parkar@gmail.com>
2020-08-12 23:12:10 +0100

    Send CORS headers

--- src/api/signin/index.js
+++ src/api/signin/index.js
@@ -34,6 +34,10 @@ async function lambdaHandler(event) {
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
       body: JSON.stringify({
         user: {
           username: userRecord.username,
--- src/api/signup/index.js
+++ src/api/signup/index.js
@@ -26,6 +26,10 @@ async function lambdaHandler(event) {
     });
     return {
       statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+      },
       body: JSON.stringify({
         user: {
           username: userRecord.username,
